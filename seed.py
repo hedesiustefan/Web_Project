22b215177360fd8f5fac3a86d8bf9eaab9700fb5
hedesiustefan <stefan.hedesiu@gmail.com>
2025-05-29 13:13:58 +0300

    multi leg routes(limited on 1 stop)

--- seed.py
+++ seed.py
@@ -22,6 +22,19 @@ for city_name in cities:
 
 db.session.commit()
 
+
+def route_exists(from_name, to_name, train_name):
+    from_city = City.query.filter_by(name=from_name).first()
+    to_city = City.query.filter_by(name=to_name).first()
+    if not from_city or not to_city:
+        return False
+    return Route.query.filter_by(
+        from_city_id=from_city.id,
+        to_city_id=to_city.id,
+        train_name=train_name
+    ).first() is not None
+
+
 # Create sample routes
 def get_city_id(name):
     return City.query.filter_by(name=name).first().id
@@ -31,19 +44,28 @@ routes = [
     ("Frankfurt", "Munich", "ICE 622", "09:00", "12:30", "3h 30m"),
     ("Cologne", "Stuttgart", "IC 2310", "11:00", "13:45", "2h 45m"),
     ("Leipzig", "Dresden", "RE 50", "10:20", "11:30", "1h 10m"),
+
+    ("Berlin", "Frankfurt", "ICE 1001", "07:00", "10:00", "3h"),
+    ("Hamburg", "Munich", "ICE 1202", "13:00", "17:00", "4h"),
+    ("Dresden", "Nuremberg", "RE 200", "15:00", "18:00", "3h"),
+    ("Stuttgart", "Leipzig", "IC 3003", "12:00", "15:00", "3h"),
+    ("Bremen", "Cologne", "RE 75", "06:30", "09:00", "2h 30m"),
+    ("Munich", "Düsseldorf", "ICE 4004", "14:30", "17:30", "3h"),
 ]
 
 for from_name, to_name, train, dep, arr, dur in routes:
-    from_id = get_city_id(from_name)
-    to_id = get_city_id(to_name)
-    db.session.add(Route(
-        from_city_id=from_id,
-        to_city_id=to_id,
-        train_name=train,
-        departure=dep,
-        arrival=arr,
-        duration=dur
-    ))
+    if not route_exists(from_name, to_name, train):
+        from_id = get_city_id(from_name)
+        to_id = get_city_id(to_name)
+        new_route = Route(
+            from_city_id=from_id,
+            to_city_id=to_id,
+            train_name=train,
+            departure=dep,
+            arrival=arr,
+            duration=dur
+        )
+        db.session.add(new_route)
 
 db.session.commit()
-print("Database seeded.")
+print("New routes added (if not already present).")
--- website/views.py
+++ website/views.py
@@ -1,29 +1,53 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from .models import City, Route
+from collections import deque
 
 
 views = Blueprint('views', __name__)
 
+def find_route_path(from_id, to_id, all_routes):
+    graph = {}
+
+    # Build adjacency list
+    for route in all_routes:
+        graph.setdefault(route.from_city_id, []).append(route)
+
+    visited = set()
+    queue = deque([[[] , from_id]])  # path_so_far, current_city
+
+    while queue:
+        path, city = queue.popleft()
+        if city == to_id:
+            return path  # full path of Route objects
+
+        if city in visited:
+            continue
+        visited.add(city)
+
+        for route in graph.get(city, []):
+            if route.to_city_id not in visited:
+                queue.append([path + [route], route.to_city_id])
+
+    return None  # No path found
+
+
 @views.route('/', methods=['GET', 'POST'])  
 @login_required
 def home():
-    print("Home route accessed by:", current_user)
-    cities = City.query.order_by(City.name).all()
-    selected_route = None
-
+    #print("Home route accessed by:", current_user)
+    selected_routes = None
+    cities = City.query.all()
+    
     if request.method == 'POST':
-        from_city_id = request.form.get('from_city')
-        to_city_id = request.form.get('to_city')
-
-        selected_route = Route.query.filter_by(
-            from_city_id=from_city_id,
-            to_city_id=to_city_id
-        ).first()
+        from_city_id = int(request.form.get('from_city'))
+        to_city_id = int(request.form.get('to_city'))
+        all_routes = Route.query.all()
+        selected_routes = find_route_path(from_city_id, to_city_id, all_routes)
 
     return render_template(
         'home.html',
         cities=cities,
-        route=selected_route,
+        route_path=selected_routes,
         user=current_user
     )
