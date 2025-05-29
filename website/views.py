6564904e11925275942eb38de5ba0fb2036d4d27
hedesiustefan <stefan.hedesiu@gmail.com>
2025-05-29 13:24:49 +0300

    n train changes possible

--- website/views.py
+++ website/views.py
@@ -6,48 +6,44 @@ from collections import deque
 
 views = Blueprint('views', __name__)
 
-def find_route_path(from_id, to_id, all_routes):
-    graph = {}
-
-    # Build adjacency list
-    for route in all_routes:
-        graph.setdefault(route.from_city_id, []).append(route)
-
+def find_route_path(from_id, to_id):
     visited = set()
-    queue = deque([[[] , from_id]])  # path_so_far, current_city
+    queue = deque()
+    queue.append((from_id, []))  # (current_city_id, path_so_far)
 
     while queue:
-        path, city = queue.popleft()
-        if city == to_id:
-            return path  # full path of Route objects
+        current_city_id, path = queue.popleft()
 
-        if city in visited:
+        if current_city_id in visited:
             continue
-        visited.add(city)
+        visited.add(current_city_id)
 
-        for route in graph.get(city, []):
-            if route.to_city_id not in visited:
-                queue.append([path + [route], route.to_city_id])
+        routes = Route.query.filter_by(from_city_id=current_city_id).all()
 
-    return None  # No path found
+        for route in routes:
+            new_path = path + [route]
+            if route.to_city_id == to_id:
+                return new_path
+            queue.append((route.to_city_id, new_path))
 
+    return None
 
 @views.route('/', methods=['GET', 'POST'])  
 @login_required
 def home():
-    #print("Home route accessed by:", current_user)
-    selected_routes = None
-    cities = City.query.all()
     
+    cities = City.query.all()
+    selected_path = None
+
     if request.method == 'POST':
         from_city_id = int(request.form.get('from_city'))
         to_city_id = int(request.form.get('to_city'))
-        all_routes = Route.query.all()
-        selected_routes = find_route_path(from_city_id, to_city_id, all_routes)
+
+        selected_path = find_route_path(from_city_id, to_city_id)
 
     return render_template(
         'home.html',
         cities=cities,
-        route_path=selected_routes,
+        path=selected_path,
         user=current_user
     )
